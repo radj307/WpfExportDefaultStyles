c42529946eed08592bcddb249dcc485f6adadeb3
radj307 <radj307@gmail.com>
2023-12-03 17:01:26 -0500

    initial code commit

---
+++ WpfControlTemplateExporter/ArgManager.cs
@@ -0,0 +1,237 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace WpfControlTemplateExporter
+{
+    public class DynamicString
+    {
+        #region Constructors
+        public DynamicString(char c) => Value = new(c, 1);
+        public DynamicString(string s) => Value = s;
+        #endregion Constructors
+
+        #region Properties
+        public string Value { get; set; }
+        [DisallowNull]
+        public char? CharValue
+        {
+            get => Value.Length == 1 ? Value[0] : null;
+            set => Value = new(value.Value, 1);
+        }
+        #endregion Properties
+
+        #region Conversion Operators
+        public static implicit operator string(DynamicString s) => s.Value;
+        public static implicit operator DynamicString(string s) => new(s);
+        public static implicit operator DynamicString(char c) => new(c);
+        #endregion Conversion Operators
+
+        #region Methods
+        public override string ToString() => Value;
+        public bool Equals(DynamicString other, StringComparison stringComparison) => Value.Equals(other.Value, stringComparison);
+        public bool Equals(DynamicString other) => Equals(other, StringComparison.Ordinal);
+        public override bool Equals(object? obj) => obj is DynamicString dynString && Equals(dynString);
+        public override int GetHashCode() => Value.GetHashCode();
+        #endregion Methods
+    }
+    public static class DynamicStringEnumerableExtensions
+    {
+        public static bool Contains(this IEnumerable<DynamicString> source, DynamicString value, StringComparison stringComparison = StringComparison.Ordinal)
+            => source.Any(item => item.Equals(value, stringComparison));
+    }
+
+    public class ArgManager
+    {
+        #region Constructors
+        public ArgManager(string[] args, params DynamicString[] implicitlyCapturingArgNames)
+        {
+            _args = Parse(args, implicitlyCapturingArgNames);
+        }
+        #endregion Constructors
+
+        #region Properties
+        public IReadOnlyList<IArg> Args => _args;
+        private readonly List<IArg> _args;
+        #endregion Properties
+
+        #region Methods
+
+        #region Parse
+        public static List<IArg> Parse(string[] args, params DynamicString[] implicitlyCapturingArgNames)
+        {
+            List<IArg> l = new();
+
+            for (int i = 0, i_max = args.Length; i < i_max; ++i)
+            {
+                var argName = args[i];
+
+                if (argName.StartsWith('-'))
+                { // non-parameter
+                    argName = argName[1..];
+
+                    // get capture arg if appended (don't need to check capturing args)
+                    string? capture = null;
+                    var eqPos = argName.IndexOf('=');
+                    if (eqPos != -1)
+                    {
+                        capture = argName[(eqPos + 1)..];
+                        argName = argName[..eqPos];
+                    }
+
+                    if (argName.StartsWith('-'))
+                    { // option
+                        argName = argName[1..];
+
+                        if (capture == null && implicitlyCapturingArgNames.Contains(argName) && i + 1 < i_max && !args[i + 1].StartsWith('-'))
+                            capture = args[++i];
+
+                        l.Add(new CaptureArg(ArgType.Option, argName, capture));
+                    }
+                    else
+                    { // flag
+                        var j_last = argName.Length - 1;
+                        // handle all chained flags except for the first one
+                        if (argName.Length > 1)
+                        {
+                            for (int j = 0; j < j_last; ++j)
+                            {
+                                l.Add(new CaptureArg(ArgType.Flag, new string(argName[j], 1)));
+                            }
+                        }
+                        // handle the last flag in the chain
+                        var flagName = new string(argName[j_last], 1);
+
+                        if (capture == null && implicitlyCapturingArgNames.Contains(flagName) && i + 1 < i_max && !args[i + 1].StartsWith('-'))
+                            capture = args[++i];
+
+                        l.Add(new CaptureArg(ArgType.Flag, flagName, capture));
+                    }
+                }
+                else
+                { // parameter
+                    l.Add(new Arg(ArgType.Parameter, argName));
+                }
+            }
+
+            return l;
+        }
+        #endregion Parse
+
+        #region GetAny
+        public IArg? GetAny(ArgType types, params DynamicString[] names)
+            => Args.FirstOrDefault(arg => types.HasFlag(arg.Type) && names.Contains(arg.Name));
+        public IArg? GetAny(params DynamicString[] names)
+            => Args.FirstOrDefault(arg => names.Contains(arg.Name));
+        #endregion GetAny
+
+        #region GetAll
+        public IEnumerable<IArg> GetAll(ArgType types)
+            => Args.Where(arg => types.HasFlag(arg.Type));
+        public IEnumerable<IArg> GetAll(ArgType types, params DynamicString[] names)
+            => Args.Where(arg => types.HasFlag(arg.Type) && names.Contains(arg.Name));
+        public IEnumerable<IArg> GetAll(params DynamicString[] names)
+            => Args.Where(arg => names.Contains(arg.Name));
+        #endregion GetAll
+
+        #region (Private) ArgToValue
+        private static string? ArgToValue(IArg? arg)
+            => arg == null ? null : arg is ICaptureArg captureArg ? captureArg.Value : arg.Name;
+        #endregion (Private) ArgToValue
+
+        #region GetAnyValue
+        public string? GetAnyValue(ArgType types, params DynamicString[] names)
+            => ArgToValue(GetAny(types, names));
+        public string? GetAnyValue(params DynamicString[] names)
+            => ArgToValue(GetAny(names));
+        #endregion GetAnyValue
+
+        #region GetAllValues
+        public IEnumerable<string> GetAllValues(ArgType types, Func<string?, bool> predicate)
+            => GetAll(types).Select(arg => arg is ICaptureArg captureArg ? captureArg.Value : arg.Name).Where(predicate).ToArray()!;
+        public IEnumerable<string> GetAllValues(ArgType types, params DynamicString[] names)
+            => GetAll(types, names).Select(arg => ArgToValue(arg)).Where(arg => arg != null)!;
+        public IEnumerable<string> GetAllValues(params DynamicString[] names)
+            => GetAll(names).Select(arg => ArgToValue(arg)).Where(arg => arg != null)!;
+        #endregion GetAllValues
+
+        #region ContainsAny
+        public bool ContainsAny(ArgType types, params DynamicString[] names)
+            => Args.Any(arg => types.HasFlag(arg.Type) && names.Contains(arg.Name));
+        public bool ContainsAny(params DynamicString[] names)
+            => Args.Any(arg => names.Contains(arg.Name));
+        #endregion ContainsAny
+
+        #region ContainsAll
+        public bool ContainsAll(ArgType types, params DynamicString[] names)
+        {
+            List<string> remainingNames = new(names.Select(name => name.ToString()));
+
+            foreach (var arg in Args)
+            {
+                if (!types.HasFlag(arg.Type)) continue;
+
+                if (remainingNames.Contains(arg.Name))
+                    remainingNames.Remove(arg.Name);
+            }
+
+            return remainingNames.Count == 0;
+        }
+        public bool ContainsAll(params DynamicString[] names)
+        {
+            List<string> remainingNames = new(names.Select(name => name.ToString()));
+
+            foreach (var arg in Args)
+            {
+                if (remainingNames.Contains(arg.Name))
+                    remainingNames.Remove(arg.Name);
+            }
+
+            return remainingNames.Count == 0;
+        }
+        #endregion ContainsAll
+
+        #endregion Methods
+
+        #region (class) Arg
+        class Arg : IArg
+        {
+            public Arg(ArgType type, string name)
+            {
+                Type = type;
+                Name = name;
+            }
+
+            public ArgType Type { get; }
+            public string Name { get; }
+
+            public override string ToString() => Name;
+        }
+        class CaptureArg : Arg, ICaptureArg
+        {
+            public CaptureArg(ArgType type, string name, string? value = null) : base(type, name)
+            {
+                Value = value;
+            }
+
+            public string? Value { get; }
+        }
+        #endregion (class) Arg
+    }
+
+    [Flags]
+    public enum ArgType : byte
+    {
+        None = 0,
+        Parameter = 1,
+        Flag = 2,
+        Option = 4,
+    }
+    public interface IArg
+    {
+        ArgType Type { get; }
+        string Name { get; }
+    }
+    public interface ICaptureArg : IArg
+    {
+        string? Value { get; }
+    }
+}
---
+++ WpfControlTemplateExporter/Program.cs
@@ -0,0 +1,201 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Xml;
+
+namespace WpfControlTemplateExporter
+{
+    internal static class Program
+    {
+        static Assembly[] LoadedAssemblies => _loadedAssemblies ??= AppDomain.CurrentDomain.GetAssemblies();
+        static Assembly[]? _loadedAssemblies = null;
+
+        static Assembly PresentationFrameworkAssembly => _presentationFrameworkAssembly ??= Assembly.GetAssembly(typeof(Control))!;
+        static Assembly? _presentationFrameworkAssembly = null;
+
+        [STAThread]
+        static int Main(string[] args)
+        {
+            var argManager = new ArgManager(args,
+                'o', "out", "output"
+            );
+
+            if (argManager.Args.Count == 0 || argManager.ContainsAny(ArgType.Flag | ArgType.Option, 'h', "help"))
+            {
+                Console.WriteLine("WPF Control Template Exporter");
+                Console.WriteLine("  Exports the default styles (including control templates) for the specified WPF control(s).");
+                Console.WriteLine("  If an output file isn't specified, outputs to STDOUT.");
+                Console.WriteLine();
+                Console.WriteLine("USAGE:");
+                Console.WriteLine($"  {Path.GetFileNameWithoutExtension(Process.GetCurrentProcess().MainModule?.FileName)} <OPTIONS> [TYPENAME...]");
+                Console.WriteLine();
+                Console.WriteLine("OPTIONS:");
+                Console.WriteLine("  -h, --help             Shows this help doc.");
+                Console.WriteLine("  -q, --quiet            Prevents messages from being written to the console.");
+                Console.WriteLine("  -o, --output <PATH>    Specifies an output filepath. You can specify multiple arguments.");
+                Console.WriteLine("  -O, --open             Opens the output file(s) in the default program.");
+                Console.WriteLine("  -i, --ignore-case      Use case-insensitive string comparisons when searching for type names.");
+                return 0;
+            }
+
+            // create an application so that the default resources can be found
+            Application app = new();
+
+            var rc = Main_impl(argManager);
+
+            app.Shutdown();
+            return rc;
+        }
+        static int Main_impl(ArgManager args)
+        {
+            TextWriter? cout = Console.Out;
+            TextWriter? cerr = Console.Error;
+
+            try
+            {
+                var outPaths = args.GetAllValues(ArgType.Flag | ArgType.Option, 'o', "out", "output").ToArray();
+
+                bool ignoreCase = args.ContainsAny(ArgType.Flag | ArgType.Option, 'i', "ignore-case");
+                var stringComparison = ignoreCase
+                    ? StringComparison.OrdinalIgnoreCase
+                    : StringComparison.Ordinal;
+
+                bool openOutputFiles = args.ContainsAny(ArgType.Flag | ArgType.Option, 'O', "open");
+
+                if (args.ContainsAny(ArgType.Flag | ArgType.Option, 'q', "quiet"))
+                {
+                    cout = null;
+                    cerr = null;
+                }
+
+                var sb = new StringBuilder();
+                using var xmlWriter = XmlWriter.Create(new StringWriter(sb), new()
+                {
+                    Encoding = Encoding.UTF8,
+                    Indent = true,
+                    ConformanceLevel = ConformanceLevel.Fragment
+                });
+                try
+                {
+                    // resolve the type names to actual types
+                    List<Type> types = new();
+                    foreach (var typeName in args.GetAll(ArgType.Parameter).Select(arg => arg.Name))
+                    {
+                        try
+                        {
+                            if (Type.GetType(typeName, throwOnError: false, ignoreCase) is Type type)
+                            {
+                                types.Add(type);
+                                cout?.WriteLine($"Successfully resolved type \"{typeName}\" => \"{type}\"");
+                            }
+                            else if (ResolveTypeName(typeName, stringComparison) is Type resolvedType)
+                            {
+                                types.Add(resolvedType);
+                                cout?.WriteLine($"Successfully resolved type \"{typeName}\" => \"{resolvedType}\"");
+                            }
+                            else cerr?.WriteLine($"[ERROR]\tFailed to resolve typename \"{typeName}\"! (Is the namespace correct?)");
+                        }
+                        catch (Exception ex)
+                        {
+                            cerr?.WriteLine($"[ERROR]\tFailed to resolve typename \"{typeName}\" due to {ex.GetType().Name}:\n{ex}");
+                        }
+                    }
+
+                    // export templates for types
+                    foreach (var type in types)
+                    {
+                        try
+                        {
+                            if (ExportDefaultControlTemplates(type, out object template))
+                            {
+                                xmlWriter.WriteComment($"  {type.AssemblyQualifiedName}  ");
+                                XamlWriter.Save(template, xmlWriter);
+                                xmlWriter.Flush();
+                                cout?.WriteLine($"Successfully retrieved template for type \"{type}\"");
+                            }
+                            else
+                            {
+                                cout?.WriteLine($"[ERROR]\tFailed to retrieve template for type \"{type}\"");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            cerr?.WriteLine($"[ERROR]\tFailed to export type \"{type.FullName}\" due to an exception:\n{ex}");
+                        }
+                    }
+                }
+                finally
+                { // output
+                    string content = sb.ToString();
+                    if (content.Length > 0)
+                    {
+                        if (outPaths.Length > 0)
+                        {
+                            foreach (var path in outPaths)
+                            {
+                                try
+                                {
+                                    File.WriteAllText(path, content);
+                                    cout?.WriteLine($"Successfully saved \"{path}\"");
+
+                                    if (openOutputFiles)
+                                    {
+                                        Process.Start(new ProcessStartInfo(path) { UseShellExecute = true })?.Dispose();
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    cerr?.WriteLine($"An exception occurred while attempting to write to \"{path}\":\n{ex}");
+                                }
+                            }
+                        }
+                        else (cout ?? Console.Out).WriteLine(content); //< override quiet for this line only
+                    }
+                    else cerr?.WriteLine("Nothing to write.");
+                }
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                cerr?.WriteLine($"[FATAL]\tThe program exited due to an exception:\n{ex}");
+                return 1;
+            }
+        }
+
+        static Type? ResolveTypeName(string typeName, StringComparison stringComparison)
+        {
+            if (PresentationFrameworkAssembly.DefinedTypes.FirstOrDefault(ti => ti.Name.Equals(typeName, stringComparison))
+                is Type t) return t;
+            // else fallback to searching all loaded assemblies
+            foreach (var assembly in LoadedAssemblies)
+            {
+                if (assembly == PresentationFrameworkAssembly) continue; //< don't search this twice
+
+                foreach (var type in assembly.DefinedTypes)
+                {
+                    if (type.Name.Equals(typeName, stringComparison))
+                    {
+                        return type;
+                    }
+                }
+            }
+            return null;
+        }
+
+        static bool ExportDefaultControlTemplates(Type type, out object template)
+        {
+            if (Application.Current.TryFindResource(type) is object resource)
+            {
+                template = resource;
+                return true;
+            }
+            template = null!;
+            return false;
+        }
+    }
+}
